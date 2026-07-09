954d9d14583e9382ab757a82ac74620c9ce5996c
Syed Riyyan Hassan <syedriyyan9@gmail.com>
2026-07-09 21:33:08 +0500

    Start with video#5. learned about django tags, templates and admin page.

--- Django/Django Practice Using Playlist/blog/views.py
+++ Django/Django Practice Using Playlist/blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
 
+posts = [{
+    'author': 'riyyan',
+    'title': 'Blog post 1',
+    'content': 'something new',
+    'date_posted': 'august 27, 2003'
+},{
+        'author': 'hassan',
+    'title': 'Blog post 2',
+    'content': 'something newer',
+    'date_posted': 'august 01, 2009'
+},{
+        'author': 'syed',
+    'title': 'Blog post 3',
+    'content': 'something',
+    'date_posted': 'octuber 27, 2003'
+}]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {'posts':posts}
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
