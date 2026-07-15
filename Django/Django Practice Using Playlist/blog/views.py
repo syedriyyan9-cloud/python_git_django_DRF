5805c4e82c737dc2e5a09d4491d23f523a39960e
riyyan <syedriyyan9@gmail.com>
2026-07-15 05:51:49 +0500

    Learned how to add log in and log out functionality in django using built in class base view from django.contrib.auth package

--- Django/Django Practice Using Playlist/blog/views.py
+++ Django/Django Practice Using Playlist/blog/views.py
@@ -25,3 +25,4 @@ def home(request):
 
 def about(request):
     return render(request, 'blog/about.html')
+
--- Django/Django Practice Using Playlist/users/views.py
+++ Django/Django Practice Using Playlist/users/views.py
@@ -2,15 +2,16 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'account created for {username}')
-            return redirect('blog-home')
+            # username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created. Log in')
+            return redirect('login')
     else:
         form = UserRegisterForm() 
     return render(request, 'users/register.html', {'form':form})
@@ -20,4 +21,8 @@ def register(request):
 # messages.success
 # messages.warning
 # messages.error
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
 
