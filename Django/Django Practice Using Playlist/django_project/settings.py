479648eedb9520612639c1404ecbd63eab3d1266
riyyan <syedriyyan9@gmail.com>
2026-07-16 05:05:32 +0500

    Learned how to upload images and create user profile when a new user signs up.

--- Django/Django Practice Using Playlist/django_project/settings.py
+++ Django/Django Practice Using Playlist/django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 LOGIN_REDIRECT_URL = "blog-home"
