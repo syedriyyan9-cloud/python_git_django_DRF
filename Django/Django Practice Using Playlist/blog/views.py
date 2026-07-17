7a389ad5917c77c4e48e144967a0ca18681df679
riyyan <syedriyyan9@gmail.com>
2026-07-17 05:40:08 +0500

    Started with cbvs and generic views in django.

--- Django/Django Practice Using Playlist/blog/views.py
+++ Django/Django Practice Using Playlist/blog/views.py
@@ -1,4 +1,9 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import (
+    ListView, DetailView, CreateView
+    )
 # from django.http import HttpResponse
 # Create your views here.
 
@@ -22,6 +27,23 @@ posts = [{
 def home(request):
     context = {'posts':posts}
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = 'Post'
+    template_name = 'blog/home.html'    # app/model_viewtype.html
+    context_object_name = 'posts'
+    # ordering = [-'date_posted']
+
+class PostDetailView(DetailView):
+    model = 'Post'
+
+class PostDetailView(CreateView):
+    model = 'Post'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html')
